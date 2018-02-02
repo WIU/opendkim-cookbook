f282a8ddb2214460035732fbcf7f1b9e6c959fd1
Matt Mencel <matt@techminer.net>
2018-02-02 15:34:31 -0600

    epel

--- metadata.rb
+++ metadata.rb
@@ -25,7 +25,7 @@ description <<-EOH
   (Domain Keys Identified Mail) sender authentication system.
 EOH
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.0' # WiP
+version '2.1.1' # WiP
 
 if respond_to?(:source_url)
   source_url "https://github.com/zuazo/#{name}-cookbook"
